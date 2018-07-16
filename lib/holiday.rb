1366e9d4ed034cc0bb59c8547a2a463a9c59da9a
Molly Seither <molly.seither@centre.edu>
2018-07-16 04:55:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,5 +52,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.map do |key, value|
+    value.map do |value, item|
+      value if item.include?("BBQ")
+    end 
+  end.flatten.compact
+end
+
 
 end
